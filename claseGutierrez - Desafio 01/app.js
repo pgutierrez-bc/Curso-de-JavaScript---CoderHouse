2179d0ae79f9e957bb8eef15e9e81378fd004950
pgutierrez - bc <pgutierrez@blockchain.com>
2022-01-18 21:25:04 -0300

    Ejercicio de algoritmo JS simple para la clase 01.

---
+++ claseGutierrez - Desafio 01/app.js
@@ -0,0 +1,4 @@
+let peliculaFavorita = prompt("Ingrese su película favorita")
+let cantidadVeces = Number(prompt("Ingrese la cantidad de veces que vio su película favorita"))
+
+let conclusion = alert("Su película favorita es " + peliculaFavorita + " y la vio " + cantidadVeces + " veces.")
